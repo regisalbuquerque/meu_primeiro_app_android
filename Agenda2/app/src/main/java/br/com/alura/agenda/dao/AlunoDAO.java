3709e3a4ff261d9eeecda070119199220a772c17
Regis Albuquerque <regis.albuquerque1@gmail.com>
2020-02-24 12:10:09 -0500

    Commit final da segunda versao do app

--- Agenda2/app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
+++ Agenda2/app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -1,5 +1,7 @@
 package br.com.alura.agenda.dao;
 
+import android.support.annotation.Nullable;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,12 +10,47 @@ import br.com.alura.agenda.model.Aluno;
 public class AlunoDAO {
 
     private final static List<Aluno> alunos = new ArrayList<>();
+    private static int contadorDeIds = 1;
 
     public void salva(Aluno aluno) {
+        aluno.setId(contadorDeIds);
         alunos.add(aluno);
+        atualizaIds();
+    }
+
+    private void atualizaIds() {
+        contadorDeIds++;
+    }
+
+    public void edita(Aluno aluno)
+    {
+        Aluno alunoEncontrado = buscaAlunoPeloid(aluno);
+        if (alunoEncontrado != null)
+        {
+            int posicaoDoAluno = alunos.indexOf(alunoEncontrado);
+            alunos.set(posicaoDoAluno, aluno);
+        }
+    }
+
+    @Nullable
+    private Aluno buscaAlunoPeloid(Aluno aluno) {
+        for (Aluno a: alunos){
+            if (a.getId() == aluno.getId())
+            {
+                return a;
+            }
+        }
+        return null;
     }
 
     public List<Aluno> todos() {
         return new ArrayList<>(alunos);
+    }
+
+    public void remove(Aluno alunoEscolhido) {
+        Aluno alunoDevolvido = buscaAlunoPeloid(alunoEscolhido);
+        if (alunoDevolvido != null) {
+            alunos.remove(alunoDevolvido);
+        }
     }
 }
--- Agenda2/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ Agenda2/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -5,17 +5,28 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+
 import br.com.alura.agenda.R;
 import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+
+import static br.com.alura.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
     public static final String TITULO_APPBAR = "Lista de alunos";
+
     private final AlunoDAO dao = new AlunoDAO();
+    private ArrayAdapter<Aluno> adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +34,29 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_alunos);
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
+        configuraLista();
+        dao.salva(new Aluno("Alex", "111111111", "alex@alura.com.br"));
+        dao.salva(new Aluno("Fran", "111111111", "fran@alura.com.br"));
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.activity_lista_alunos_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+        if(itemId == R.id.activity_lista_alunos_alunos_menu_remover){
+            AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
+            remove(alunoEscolhido);
+        }
+
+
+        return super.onContextItemSelected(item);
     }
 
     private void configuraFabNovoAluno() {
@@ -30,26 +64,74 @@ public class ListaAlunosActivity extends AppCompatActivity {
         botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                abreFormularioAlunoActivity();
+                abreFormularioModoInsereAluno();
             }
         });
     }
 
-    private void abreFormularioAlunoActivity() {
+    private void abreFormularioModoInsereAluno() {
         startActivity(new Intent(this, FormularioAlunoActivity.class));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        configuraLista();
+        atualizaAlunos();
+    }
+
+    private void atualizaAlunos() {
+        adapter.clear();
+        adapter.addAll(dao.todos());
     }
 
     private void configuraLista() {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+        configuraAdapter(listaDeAlunos);
+        configuraListenerDeClickPorItem(listaDeAlunos);
+        //configuraListenerDeCliqueLongoPorItem(listaDeAlunos);
+        registerForContextMenu(listaDeAlunos);
+    }
+
+//    private void configuraListenerDeCliqueLongoPorItem(ListView listaDeAlunos) {
+//        listaDeAlunos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+//                Log.i("clique longo", " " + posicao);
+//                Aluno alunoEscolhido = (Aluno) adapterView.getItemAtPosition(posicao);
+//                remove(alunoEscolhido);
+//                return false;
+//            }
+//        });
+//    }
+
+    private void remove(Aluno aluno) {
+        dao.remove(aluno);
+        adapter.remove(aluno);
+    }
+
+    private void configuraListenerDeClickPorItem(ListView listaDeAlunos) {
+        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Aluno alunoEscolhido = (Aluno) adapterView.getItemAtPosition(posicao);
+                Log.i("aluno", " " + alunoEscolhido);
+                abreFormularioModoEditaAluno(alunoEscolhido);
+            }
+        });
+    }
+
+
+
+    private void abreFormularioModoEditaAluno(Aluno aluno) {
+        Intent vaiParaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class);
+        vaiParaFormularioActivity.putExtra(CHAVE_ALUNO,aluno);
+        startActivity(vaiParaFormularioActivity);
+    }
+
+    private void configuraAdapter(ListView listaDeAlunos) {
+        adapter = new ArrayAdapter<>(
                 this,
-                android.R.layout.simple_list_item_1,
-                dao.todos()));
+                android.R.layout.simple_list_item_1);
+        listaDeAlunos.setAdapter(adapter);
     }
 }
