9a520675fe7a833c1757b09f2ae4d5b9c0db0bae
Regis Albuquerque <regis.albuquerque1@gmail.com>
2020-03-11 15:22:27 -0500

    Commit final da terceira versao do app

---
+++ AluraViagens/app/src/main/java/br/com/alura/aluraviagens/ResourceUtil.java
@@ -0,0 +1,19 @@
+package br.com.alura.aluraviagens;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+
+import br.com.alura.aluraviagens.model.Pacote;
+
+public class ResourceUtil {
+
+    public static final String DRAWABLE = "drawable";
+
+    public static Drawable devolveDrawable(Context context, String drawableEmTexto) {
+        Resources resources = context.getResources();
+        int idDoDrawable = resources.getIdentifier(drawableEmTexto
+                , DRAWABLE, context.getPackageName());
+        return resources.getDrawable(idDoDrawable);
+    }
+}
